3dcc2e16f56bedc287b53915773623bf51cfa135
X CroSs <khsumate@gmail.com>
2025-11-29 22:41:07 +0700

    Add health check and node version

--- server/index.js
+++ server/index.js
@@ -12,6 +12,10 @@ const io = new Server(server, {
     }
 });
 
+app.get('/', (req, res) => {
+    res.send('TikTok Live Monitor Backend is Running!');
+});
+
 let tiktokConnection = null;
 let currentUsername = null;
 let isConnecting = false;
